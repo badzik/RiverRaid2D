9f6d10837beebc5a63c7e4adf21c8fa78d7aae86
Bartosz Sosnowski <bartosz-sosnowski@o2.pl>
2017-04-01 21:23:49 +0200

    Changed grass drawing

--- Assets/GroundLooperScript.cs
+++ Assets/GroundLooperScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using UnityEditor;
 
 public class GroundLooperScript : MonoBehaviour
 {
@@ -58,14 +59,16 @@ public class GroundLooperScript : MonoBehaviour
     private void create(Collider2D collider)
     {
         Vector2 boxPosition = ((BoxCollider2D)collider).transform.position;
-        float startx,startx2;
+        float startx, startx2;
         float posx, posy;
         float sizex, sizey;
         float diffy;
         float diffx;
         int i = 0;
+        Vector2[] leftTriangle = new Vector2[3];
+        Vector2[] rightTriangle = new Vector2[3];
         startx = boxPosition.x - ((BoxCollider2D)collider).size.x / 2; //dla strony lewej
-        startx2= boxPosition.x + ((BoxCollider2D)collider).size.x / 2; //dla strony prawej
+        startx2 = boxPosition.x + ((BoxCollider2D)collider).size.x / 2; //dla strony prawej
         diffx = ((BoxCollider2D)collider).size.x / scale;
         diffy = ((BoxCollider2D)collider).size.y / boxParts;
         posy = boxPosition.y - (((BoxCollider2D)collider).size.y / 2) + (diffy / 2);
@@ -76,57 +79,135 @@ public class GroundLooperScript : MonoBehaviour
         }
         for (; i < tab.Length; i++)
         {
-                //lewa strona
-                posx = startx + (tab[i][0] * diffx) / 2;
-                sizex = tab[i][0] * diffx;
-                sizey = diffy;
-                GameObject leftGrass = new GameObject();
-                leftGrass.tag = "Terrain";
-                BoxCollider2D c1 = leftGrass.AddComponent<BoxCollider2D>();
-                leftGrass.transform.position = new Vector2(posx, posy);
-                leftGrass.transform.localScale = new Vector3(sizex, sizey);
-                fillWithGrass(leftGrass);
+            //lewa strona
+            posx = startx + (tab[i][0] * diffx) / 2;
+            sizex = tab[i][0] * diffx;
+            sizey = diffy;
+            GameObject leftGrass = new GameObject();
+            leftGrass.tag = "Terrain";
+            SpriteRenderer r1 = leftGrass.AddComponent<SpriteRenderer>();
+            r1.sprite = Resources.Load("Sprites/Square", typeof(Sprite)) as Sprite;
+            r1.material = Resources.Load("Sprites/Grass", typeof(Material)) as Material;
+            BoxCollider2D c1 = leftGrass.AddComponent<BoxCollider2D>();
+            leftGrass.transform.position = new Vector2(posx, posy);
+            leftGrass.transform.localScale = new Vector3(sizex, sizey);
+            //obliczanie wspolrzednych trojkatow
+            if (i < tab.Length - 1)
+            {
+                if (tab[i][0] != tab[i + 1][0])
+                {
+                    if (tab[i][0] > tab[i + 1][0])
+                    {
+                        leftTriangle[0] = new Vector2(posx + (sizex / 2), posy + (sizey / 2));
+                        leftTriangle[1] = new Vector2((startx + (tab[i + 1][0] * diffx) / 2) + ((tab[i+1][0] * diffx)/2),posy+diffy-(sizey/2));
+                        leftTriangle[2] = new Vector2((startx + (tab[i + 1][0] * diffx) / 2) + ((tab[i + 1][0] * diffx)/2), posy + diffy + (sizey / 2));
+                    }
+                    else
+                    {
+                        leftTriangle[0] = new Vector2(posx + (sizex / 2), posy - (sizey / 2));
+                        leftTriangle[1] = new Vector2(posx + (sizex / 2), posy + (sizey / 2));
+                        leftTriangle[2] = new Vector2((startx + (tab[i + 1][0] * diffx) / 2) + ((tab[i + 1][0] * diffx)/2), posy + diffy - (sizey / 2));
+                    }
+                }
+            }
 
-                //prawa strona
-                posx = startx2 - (tab[i][0] * diffx) / 2;
-                sizex = tab[i][0] * diffx;
-                sizey = diffy;
-                GameObject rightGrass = new GameObject();
-                rightGrass.tag = "Terrain";
-                BoxCollider2D c2 = rightGrass.AddComponent<BoxCollider2D>();
-                rightGrass.transform.position = new Vector2(posx, posy);
-                rightGrass.transform.localScale = new Vector3(sizex, sizey);
-                fillWithGrass(rightGrass);
 
-                posy += diffy;
+            //prawa strona
+            posx = startx2 - (tab[i][0] * diffx) / 2;
+            sizex = tab[i][0] * diffx;
+            sizey = diffy;
+            GameObject rightGrass = new GameObject();
+            rightGrass.tag = "Terrain";
+            SpriteRenderer r2 = rightGrass.AddComponent<SpriteRenderer>();
+            r2.sprite = Resources.Load("Sprites/Square", typeof(Sprite)) as Sprite;
+            r2.material = Resources.Load("Sprites/Grass", typeof(Material)) as Material;
+            BoxCollider2D c2 = rightGrass.AddComponent<BoxCollider2D>();
+            rightGrass.transform.position = new Vector2(posx, posy);
+            rightGrass.transform.localScale = new Vector3(sizex, sizey);
+            //obliczanie wspolrzednych trojkatow
+            if (i < tab.Length - 1)
+            {
+                if (tab[i][0] != tab[i + 1][0])
+                {
+                    if (tab[i][0] > tab[i + 1][0])
+                    {
+                        rightTriangle[0] = new Vector2(posx - (sizex / 2), posy + (sizey / 2));
+                        rightTriangle[1] = new Vector2((startx2 - (tab[i + 1][0] * diffx) / 2) - ((tab[i + 1][0] * diffx) / 2), posy + diffy - (sizey / 2));
+                        rightTriangle[2] = new Vector2((startx2 - (tab[i + 1][0] * diffx) / 2) - ((tab[i + 1][0] * diffx) / 2), posy + diffy + (sizey / 2));
+                    }
+                    else
+                    {
+                        rightTriangle[0] = new Vector2(posx - (sizex / 2), posy - (sizey / 2));
+                        rightTriangle[1] = new Vector2(posx - (sizex / 2), posy + (sizey / 2));
+                        rightTriangle[2] = new Vector2((startx2 - (tab[i + 1][0] * diffx) / 2) - ((tab[i + 1][0] * diffx) / 2), posy + diffy - (sizey / 2));
+                    }
+                }
+            }
+
+            //rysowanie trojkatow
+
+            if (leftTriangle[0].x != 0.0 && leftTriangle[0].y != 0.0)
+            {
+                GameObject tr1 = new GameObject();
+                tr1.tag = "Terrain";
+                MeshFilter trFilter = tr1.AddComponent<MeshFilter>();
+                MeshRenderer trRenderer = tr1.AddComponent<MeshRenderer>();
+                trRenderer.material = Resources.Load("Sprites/Grass", typeof(Material)) as Material;
+                Mesh trMesh = tr1.GetComponent<MeshFilter>().mesh;
+                trMesh.vertices = new Vector3[3] { new Vector3(leftTriangle[0].x, leftTriangle[0].y), new Vector3(leftTriangle[1].x, leftTriangle[1].y), new Vector3(leftTriangle[2].x, leftTriangle[2].y) };
+                trMesh.uv = new Vector2[3] { leftTriangle[0], leftTriangle[1], leftTriangle[2] };
+                trMesh.triangles = new int[] { 0, 1, 2 };
+                PolygonCollider2D pc = tr1.AddComponent<PolygonCollider2D>();
+                pc.points = new Vector2[3] { leftTriangle[0], leftTriangle[1], leftTriangle[2] };
+            }
+
+
+            if (rightTriangle[0].x != 0.0 && rightTriangle[0].y != 0.0)
+            {
+                GameObject tr2 = new GameObject();
+                tr2.tag = "Terrain";
+                MeshFilter trFilter2 = tr2.AddComponent<MeshFilter>();
+                MeshRenderer trRenderer2 = tr2.AddComponent<MeshRenderer>();
+                trRenderer2.material = Resources.Load("Sprites/Grass", typeof(Material)) as Material;
+                Mesh trMesh2 = new Mesh();
+                trMesh2.Clear();
+                trMesh2.vertices = new Vector3[] { new Vector3(rightTriangle[0].x, rightTriangle[0].y), new Vector3(rightTriangle[1].x, rightTriangle[1].y), new Vector3(rightTriangle[2].x, rightTriangle[2].y) };
+                trMesh2.uv = new Vector2[] { rightTriangle[0], rightTriangle[1], rightTriangle[2] };
+                trMesh2.triangles = new int[] { 0, 1, 2 };
+                tr2.GetComponent<MeshFilter>().mesh = trMesh2;
+            }
+
+
+            posy += diffy;
         }
         for (i = 0; i < tab.Length; i++)
         {
-            tab[i][0] = random.Next(2,7);
+            tab[i][0] = random.Next(2, 7);
             tab[i][1] = 0;
         }
     }
 
+    //nie używać!
     private void fillWithGrass(GameObject area)
     {
-        Sprite sprite = Resources.Load<Sprite>("Sprites/grasspart");
+        Sprite sprite = Resources.Load<Sprite>("Sprites/16pxgrass");
         Vector2 spriteSize = sprite.rect.size;
         float startx, starty;
         float endx, endy;
         float temp = area.GetComponent<BoxCollider2D>().size.x;
         float temp2 = area.GetComponent<BoxCollider2D>().size.y;
-        startx = (area.transform.position.x - (area.transform.localScale.x / 2.0f)) + (spriteSize.x/200.0f);
-        starty= (area.transform.position.y + (area.transform.localScale.y / 2.0f)) - (spriteSize.y/200.0f);
+        startx = (area.transform.position.x - (area.transform.localScale.x / 2.0f)) + (spriteSize.x / 200.0f);
+        starty = (area.transform.position.y + (area.transform.localScale.y / 2.0f)) - (spriteSize.y / 200.0f);
         endx = (area.transform.position.x + (area.transform.localScale.x / 2.0f));
-        endy= (area.transform.position.y - (area.transform.localScale.y / 2.0f));
-        for(float y=starty;y>=(endy- (spriteSize.y / 100.0f));y-=(spriteSize.y/100.0f))
+        endy = (area.transform.position.y - (area.transform.localScale.y / 2.0f));
+        for (float y = starty; y >= (endy); y -= (spriteSize.y / 100.0f))
         {
             for (float x = startx; x <= endx; x += (spriteSize.x / 100.0f))
             {
                 GameObject grass = new GameObject();
                 grass.tag = "Terrain";
                 SpriteRenderer r = grass.AddComponent<SpriteRenderer>();
-                r.sprite = Resources.Load("Sprites/grasspart", typeof(Sprite)) as Sprite;
+                r.sprite = Resources.Load("Sprites/16pxgrass", typeof(Sprite)) as Sprite;
                 BoxCollider2D c1 = grass.AddComponent<BoxCollider2D>();
                 c1.isTrigger = true;
                 grass.transform.position = new Vector2(x, y);
@@ -155,7 +236,7 @@ public class GroundLooperScript : MonoBehaviour
                 c.isTrigger = true;
                 grass.transform.position = new Vector2(tempx, tempy);
                 SpriteRenderer renderer = grass.AddComponent<SpriteRenderer>();
-                renderer.sprite = Resources.Load("Sprites/grasspart", typeof(Sprite)) as Sprite;
+                renderer.sprite = Resources.Load("Sprites/16pxgrass", typeof(Sprite)) as Sprite;
                 tempx += 0.1f;
             }
             tempy += 0.1f;
